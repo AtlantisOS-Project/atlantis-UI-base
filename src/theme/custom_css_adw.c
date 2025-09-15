cc438cf6a9af720908b182a3dfdaddb3bb90cdfb
NachtsternBuild <hallo.schneller50kmh@gmail.com>
2025-09-15 12:34:20 +0200

    improve style
    rework the style provided by libadwaita and CSS.

--- src/theme/custom_css_adw.c
+++ src/theme/custom_css_adw.c
@@ -29,45 +29,41 @@ const char *get_custom_adw_css(void)
     return 
  		"window {\n"
     	"    background-color: @theme_bg_color;\n" // bg color - 1
-    	"    border: 2px solid @accent_bg_color;\n" // accent color - 2
+    	"    color: @theme_fg_color;\n" // fg color - 2
+    	"    padding: 12px;\n"
+    	"    border: 2px solid @theme_bg_color;\n" // bg color - 3
     	"    border-radius: 35px;\n"
-    	"    padding: 12px 24px;\n"
-    	"    color: @theme_fg_color;\n" // fg color - 3
-    	"    font-size: 16px;\n"
     	"}\n"
     	"button {\n"
-    	"    border: 2px solid @accent_bg_color;\n" // accent color - 4
+    	"    background-color: @accent_bg_color;\n" // bg color - 4
+    	"    color: @theme_fg_color;\n" // text / fg color - 5
+    	"    padding: 12px;\n"
+    	"    border: 2px solid @theme_bg_color;\n" // bg color - 6
     	"    border-radius: 35px;\n"
-    	"    padding: 24px 48px;\n"
-    	"    background-color: @theme_bg_color;\n" // bg color - 5
-    	"    color: @theme_fg_color;\n" // text / fg color - 6 
-    	"    font-size: 16px;\n"
     	"}\n"
     	"button:hover {\n"
-  		"	background-color: @accent_color;\n"
-  		"	color: @accent_fg_color;\n"
-  		"	transition: background-color 150ms ease;\n"
+  		"	 background-color: @accent_color;\n" // accent color - 7
+  		"	 color: @accent_fg_color;\n" // fg color - 8
+  		"	 transition: background-color 150ms ease;\n"
 		"}\n"
     	"label {\n"
-    	"    color: @theme_fg_color;\n" // text / fg color - 8
-    	"    font-size: 16px;\n"
+    	"    color: @theme_fg_color;\n" // text / fg color - 9
     	"    font-weight: bold;\n"
     	"}\n"
     	"headerbar {\n"
-    	"    background-color: @theme_bg_color;\n" // bg color - 15
+    	"    background-color: @theme_bg_color;\n" // bg color - 10
     	"    font-weight: bold;\n"
-    	"    border: 2px solid @accent_bg_color;\n" // accent color - 16
+    	"    color: @theme_fg_color;\n" // text / fg color - 11
+    	"    padding: 12px;\n"
+    	"    border: 2px solid @theme_bg_color;\n" // bg color - 12
     	"    border-radius: 35px;\n"
-    	"    padding: 12px 24px;\n"
-    	"    color: @theme_fg_color;\n" // text / fg color - 17
-    	"    font-size: 16px;\n"
     	"}\n"
     	"frame {\n"
-    	"	border-width: 5px;\n"
-    	"	border-radius: 35px;\n"
-		"   border-color: @accent_bg_color;\n" // accent color - 18
-    	"	border-style: solid;\n"
-    	"	padding: 10px;\n"
+    	"	 border-width: 5px;\n"
+    	"	 border-radius: 35px;\n"
+		"    border-color: @accent_bg_color;\n" // accent color - 13
+    	"	 border-style: solid;\n"
+    	"	 padding: 10px;\n"
 		"}\n";
 }			
 	
